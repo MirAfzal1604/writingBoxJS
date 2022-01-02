0a19f585206d01aaa874d33c2b262724a6fab7db
MirAfzal <92784299+MirAfzal1604@users.noreply.github.com>
2022-01-02 15:08:02 +0500

    Update write.js

--- write.js
+++ write.js
@@ -15,11 +15,5 @@ function txtsetCount() {
     const txtCoundResult = textareaLength - textareaAll.value.length;
     words.innerHTML = txtCoundResult;
 
-    if (txtCoundResult > 50) {
-        words.style.color = "#00cc00";
-    } else if (txtCoundResult > 21) {
-        words.style.color = "gold";
-    } else {
-        words.style.color = "red";
-    }
+    txtCoundResult > 50 ? words.style.color = "#00cc00" : txtCoundResult > 21 ? words.style.color = "gold" : words.style.color = "red";
 };
